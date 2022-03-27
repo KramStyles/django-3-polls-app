8b9adeac86d503f512bcf00592f1e4e133604610
kramstyles <mark.eke@decagon.dev>
2022-03-27 13:31:53 +0100

    added polls views

--- polls_app/polls/urls.py
+++ polls_app/polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('<int:ques_id>', views.details, name='detail'),
+    path('<int:ques_id>/results', views.results, name='result'),
+    path('<int:ques_id>/vote', views.vote, name='votes')
 ]
--- polls_app/polls/views.py
+++ polls_app/polls/views.py
@@ -3,3 +3,18 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h2>Hello and Welcome to the Polls app</h2>")
+
+
+def details(request, ques_id):
+    response = "<h2>You are looking at Question: %s</h2>" % ques_id
+    return HttpResponse(response)
+
+
+def results(request, ques_id):
+    response = "<h2>You are looking at results of Question: %s</h2>"
+    return HttpResponse(response % ques_id)
+
+
+def vote(request, ques_id):
+    response = "<h3>You are voting on question: %s</h3>" % ques_id
+    return HttpResponse(response)
