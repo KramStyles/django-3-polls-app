7420432560bb2c92b64450f117820348eec2d747
kramstyles <mark.eke@decagon.dev>
2022-03-28 00:31:35 +0100

    used generic List and Detail views

--- polls_app/polls/urls.py
+++ polls_app/polls/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('<int:ques_id>', views.details, name='detail'),
-    path('<int:ques_id>/results', views.results, name='result'),
-    path('<int:ques_id>/vote', views.vote, name='vote')
+    # path('', views.index, name='home'),
+    # path('<int:ques_id>', views.details, name='detail'),
+    # path('<int:ques_id>/results', views.results, name='result'),
+    path('', views.IndexView.as_view(), name='home'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='result'),
+    path('<int:pk>/', views.DetailsView.as_view(), name='detail'),
+    path('<int:ques_id>/vote/', views.vote, name='vote')
 ]
