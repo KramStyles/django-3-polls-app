a70e1301302df064c2054b6250b4818096d56ec7
kramstyles <mark.eke@decagon.dev>
2022-03-27 20:34:54 +0100

    fixed url issues and added vote functionality

--- polls_app/polls/urls.py
+++ polls_app/polls/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='home'),
     path('<int:ques_id>', views.details, name='detail'),
     path('<int:ques_id>/results', views.results, name='result'),
-    path('<int:ques_id>/vote', views.vote, name='votes')
+    path('<int:ques_id>/vote', views.vote, name='vote')
 ]
--- polls_app/polls/views.py
+++ polls_app/polls/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
+from django.urls import reverse
+# from django.views import generic
 
 from .models import Question, Choice
 
@@ -20,7 +22,7 @@ def details(request, ques_id):
             'question': question,
             'id': ques_id
         }
-        template = loader.get_template('polls/details.html')
+        template = loader.get_template('polls/details.jinja2')
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return HttpResponse(template.render(context, request))
@@ -33,5 +35,18 @@ def results(request, ques_id):
 
 
 def vote(request, ques_id):
+    question = get_object_or_404(Question, pk=ques_id)
+    try:
+        selected = question.choice_set.get(pk=request.POST['choice'])
+        selected.votes += 1
+        selected.save()
+
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
+    except (KeyError, Choice.DoesNotExist):
+        context = {
+            'question': question,
+            'error_msg': "You didn't select a choice"
+        }
+        render(request, 'polls/details.html', context)
     response = "<h3>You are voting on question: %s</h3>" % ques_id
     return HttpResponse(response)
