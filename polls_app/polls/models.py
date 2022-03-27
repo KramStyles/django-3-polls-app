edfd897435a95158895f939eb9c9cf4017dec176
kramstyles <mark.eke@decagon.dev>
2022-03-27 11:05:25 +0100

    added model codes and ran python shell queries

--- polls_app/polls/models.py
+++ polls_app/polls/models.py
@@ -1,12 +1,25 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     txt_question = models.CharField(max_length=200)
     published_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.txt_question
+
+    def recently_published(self):
+        print(self.published_date)
+        return self.published_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     txt_choice = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.txt_choice
