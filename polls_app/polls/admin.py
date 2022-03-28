0fb95ba04c01612cac8947cfb0105847fbd473ba
kramstyles <mark.eke@decagon.dev>
2022-03-28 19:15:09 +0100

    introduced tabularinline admin and list display

--- polls_app/polls/admin.py
+++ polls_app/polls/admin.py
@@ -3,12 +3,14 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInLine(admin.StackedInline):
+class ChoiceInLine(admin.TabularInline):
+    # You can use admin.StackInline to stack
     model = Choice
     extra = 1
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    # To change the order of columns
     # fields = ['published_date', 'txt_question']
 
     # To show only one or certain fields
@@ -19,6 +21,8 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date Information', {'fields': ['published_date']})
     ]
     inlines = [ChoiceInLine]
+
+    list_display = ('txt_question', 'published_date', 'recently_published')
 
 
 admin.site.register(Question, QuestionAdmin)
