2dba254cdb508c7de22f1faf6c94b8b1ada06cd7
kramstyles <mark.eke@decagon.dev>
2022-03-28 15:36:16 +0100

    used fieldset

--- polls_app/polls/admin.py
+++ polls_app/polls/admin.py
@@ -4,10 +4,15 @@ from .models import Question, Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['published_date', 'txt_question']
-    
+    # fields = ['published_date', 'txt_question']
+
     # To show only one or certain fields
     # fields = ['txt_question']
+
+    fieldsets = [
+        ('Question', {'fields': ['txt_question']}),
+        ('Date Information', {'fields': ['published_date']})
+    ]
 
 
 admin.site.register(Question, QuestionAdmin)
