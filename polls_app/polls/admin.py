68ee1c375d75974b8dfdc7065fcc781a4d75fcb6
kramstyles <mark.eke@decagon.dev>
2022-03-28 15:28:20 +0100

    organized column order

--- polls_app/polls/admin.py
+++ polls_app/polls/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
 from .models import Question, Choice
-# Register your models here.
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['published_date', 'txt_question']
+    
+    # To show only one or certain fields
+    # fields = ['txt_question']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
