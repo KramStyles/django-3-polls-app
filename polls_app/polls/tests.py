c5e14a22a09d26a8d4a03abe5f7fad4a970474ce
kramstyles <mark.eke@decagon.dev>
2022-03-30 08:04:34 +0100

    added more  recently published tests

--- polls_app/polls/tests.py
+++ polls_app/polls/tests.py
@@ -11,3 +11,13 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_ = Question(published_date=time)
         self.assertIs(future_.recently_published(), False)
+
+    def test_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(published_date=time)
+        self.assertIs(old_question.recently_published(), False)
+
+    def test_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=14)
+        question = Question(published_date=time)
+        self.assertTrue(question.recently_published())
